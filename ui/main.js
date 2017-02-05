1aa54c7340407b680a3708ac26ceffb1c5f4db2c
rajessaravanan <rajes.saravanan@gmail.com>
2017-02-05 23:05:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,9 +9,9 @@ element.innerHTML = 'New Value for Saravana';
 var img = document.getElementById('madi')
 var marginLeft = 0;
 function moveRight(){
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function(){
-    var interval = setinterval(moveRight, 100);
+    var interval = setinterval(moveRight, 50);
 };
