50402fa5efd072951af817592cbf54dc0208c1f8
rajessaravanan <rajes.saravanan@gmail.com>
2017-02-05 15:23:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req,res){
-    res.send('article one will beserved here');
+app.get('/saravana-one', function (req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'saravana-one.html'));
 });
 
 app.get('/article-two', function (req,res){
